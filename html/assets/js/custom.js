2b65d7f4673501990e094c8f312e1d70b491099e
Udyam Kumar <udyam.kumar@ixcnew.com>
2020-04-29 04:58:58 +0530

    Anda Website html

---
+++ html/assets/js/custom.js
@@ -0,0 +1,6 @@
+// Navigation responsive Javascript
+function navFunction() {
+        var element = document.getElementById("header-nav");
+        element.classList.toggle("header-collapse");
+    
+ }
